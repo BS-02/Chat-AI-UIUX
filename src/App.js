ffd947bd42418f1a607f71a18347b85618dd0520
BS-02 <127045845+BS-02@users.noreply.github.com>
2024-07-10 13:50:42 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Footer, Possibility, Features, Header } from './containers';
+import { CTA, Navbar } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import Case from './containers/case/Case';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    {/* <Brand /> */}
+    <Case />
+    <Features />
+    <Possibility />
+    <CTA />
+    {/* <Blog /> */}
+    <Footer />
+  </div>
+);
 
 export default App;
